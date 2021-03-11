aaef8e8b55c951b22282a019761723155aaa2f6b
niclad <nicktedori@gmail.com>
2021-03-11 21:37:44 +0000

    Include algorithm skeleton

---
+++ model/LoadBalancing.cpp
@@ -0,0 +1,4 @@
+#include "LoadBalancing.h"
+
+void
+lba::roundrobin() {}
---
+++ model/LoadBalancing.h
@@ -0,0 +1,11 @@
+#ifndef LOAD_BALANCING_ALGO_H
+#define LOAD_BALANCING_ALGO_H
+
+namespace lba {
+void roundrobin();
+void a();
+void b();
+void c();
+}  // namespace lba
+
+#endif
