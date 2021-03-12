2250e49cb02b817d9c0869575663b8a8b35eac76
niclad <nicktedori@gmail.com>
2021-03-12 00:11:56 +0000

    set up initial Dispatcher design

--- model/Dispatcher.cpp
+++ model/Dispatcher.cpp
@@ -1,16 +1,6 @@
 #include "Dispatcher.h"
 
-Dispatcher::Dispatcher(void (*func)(int job), std::vector<ServiceNode> nodes) 
-  : lbFunc{func}, nodeList{nodes} 
-{
+Dispatcher::Dispatcher(int (*func)(int), std::vector<ServiceNode> nodes)
+    : lbFunc{func}, nodeList{nodes} {}
 
-}
-
-Dispatcher::~Dispatcher() {
- 
-}
-
-void
-Dispatcher::dispatchJob(int job) {
-  lbFunc(job);
-}
+int Dispatcher::dispatchJob(int job) { return lbFunc(job); }
--- model/Dispatcher.h
+++ model/Dispatcher.h
@@ -5,15 +5,30 @@
 
 #include "Node.h"
 
+/* NOTE: "int job" is a place holder until a job can be better defined
+ */
+
 class Dispatcher {
 public:
-  Dispatcher(void (*func)(), std::vector<ServiceNode> nodes);
-  ~Dispatcher();
-  void dispatchJob(int job);
+  /**
+   * @brief Construct a new Dispatcher object
+   * 
+   * @param func The load-balancing function to use for the dispatching
+   * @param nodes The list of the nodes that are in the model
+   */
+  Dispatcher(int (*func)(int), std::vector<ServiceNode> nodes);
+
+  /**
+   * @brief Dispatch a job to a node based on the lbFunc.
+   * 
+   * @param job The job to dispatch.
+   * @return int The node where the job was sent.
+   */
+  int dispatchJob(int job);
 
 private:
   // pointer to a defined load-balancing function
-  void (*lbFunc)(int job);
+  int (*lbFunc)(int);
 
   // list holding all the service nodes in the model
   std::vector<ServiceNode> nodeList;
