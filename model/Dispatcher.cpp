5567952ea37f055582d046bec6af42c8998cc130
niclad <nicktedori@gmail.com>
2021-03-12 17:44:51 +0000

    remove redundant objects

--- model/Dispatcher.cpp
+++
@@ -1,6 +0,0 @@
-#include "Dispatcher.h"
-
-Dispatcher::Dispatcher(int (*func)(std::vector<ServiceNode>), std::vector<ServiceNode> nodes)
-    : lbFunc{func}, nodeList{nodes} {}
-
-int Dispatcher::dispatchJob() { return lbFunc(nodeList); }
