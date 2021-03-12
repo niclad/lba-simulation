60b29c38c71c517a55fb6d88e5331f10c950abdb
niclad <nicktedori@gmail.com>
2021-03-12 17:17:01 +0000

    test the current implementations

--- model/temp.cpp
+++ model/temp.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <vector>
 
 #include "Dispatcher.h"
 #include "LoadBalancing.h"
 #include "Node.h"
 
 int main() {
+  std::vector<ServiceNode> nodes = {};
+  for (int i = 0; i < 10; i++) {
+    nodes.push_back(ServiceNode(i));
+  }
+  Dispatcher test(&lba::testLBA, nodes);
+
+  test.dispatchJob();
+
   std::cout << "Hello, world" << std::endl;
 }
