82ae765f66de04c1a9fd2031344797f1de47bb02
tyler royere <royere.t@northeastern.edu>
2021-03-13 14:39:04 -0500

    Defined some load balancing algorithms, added rng files, created simple tests, updated makefile

--- model/LoadBalancing.cpp
+++ model/LoadBalancing.cpp
@@ -1,8 +1,52 @@
 #include "LoadBalancing.h"
+#include "rvgs.h"
 
 #include <iostream>
 
-int lba::roundrobin(std::vector<ServiceNode> nodeList) { return -1; }
+int lba::roundrobin(std::vector<ServiceNode> nodeList) { 
+    // static counter of jobs
+    static int index{0};
+
+    int server = index;
+
+    // update index accounting for node_size
+    index++;
+    index %= nodeList.size();
+
+    return server;
+}
+
+int lba::random(std::vector<ServiceNode> nodeList) {
+    // return a random server index
+    return Equilikely(0, nodeList.size()-1);
+}
+
+int lba::utilizationbased(std::vector<ServiceNode> nodeList) {
+    int least_utilized{0};
+
+    // find the index with the least utilization
+    for (size_t ii = 1; ii < nodeList.size(); ii++) {
+        if (nodeList[least_utilized].getUtil() >
+                nodeList[ii].getUtil()) {
+            least_utilized = ii;
+        }
+    }
+    return least_utilized;
+} 
+
+int lba::leastconnections(std::vector<ServiceNode> nodeList) {
+    int least_connections{0};
+
+    // find the index with the least number of jobs
+    for (size_t ii = 1; ii < nodeList.size(); ii++) {
+        if (nodeList[least_connections].getQueueLength() <
+                nodeList[ii].getQueueLength()) {
+            least_connections = ii;
+        }
+    }
+
+    return least_connections;
+}
 
 int lba::testLBA(std::vector<ServiceNode> nodeList) {
   if (nodeList.size() > 0) {
