e6d5f6b6974a3c34699fd48512d3dadad49af2b9
niclad <nicktedori@gmail.com>
2021-03-29 16:32:00 +0000

    set delay to 0 in no prev departure case

--- model/Job.cpp
+++ model/Job.cpp
@@ -1,20 +1,13 @@
 #include "Job.h"
-
 #include "rvgs.h"
 
-Job::Job(double arrival) : arrival{arrival}, service{getService()} {
+Job::Job(double arrival) : arrival{arrival}, service{getService()}, delay{0} {
   // service needs to be generated via GetService
 }
 
 Job::Job(double arrival, double prevDeparture)
     : arrival{arrival}, service{getService()}, delay{setDelay(prevDeparture)} {
   // service needs to be generated via GetService
-}
-
-double Job::getService() {
-  // get an exponential random variate in seconds
-  return Exponential(
-      4049);  // 4049 sec. is mean random service time on discovery
 }
 
 double Job::setDelay(double prevDeparture) {
@@ -25,6 +18,12 @@ double Job::setDelay(double prevDeparture) {
   }
 
   return delay;
+}
+
+double Job::getService() {
+  // get an exponential random variate in seconds
+  return Exponential(
+      4049);  // 4049 sec. is mean random service time on discovery
 }
 
 double Job::calcWait() { return delay + service; }
