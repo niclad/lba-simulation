c4345cc04d8e032f9fe2577a0e050826c772d608
niclad <nicktedori@gmail.com>
2021-03-30 16:50:03 +0000

    add sqms creation

--- model/main.cpp
+++ model/main.cpp
@@ -12,14 +12,27 @@ const int NOON_TIME{DAY_SEC / 2};        // time of day for noon
 const double START{0.0};                 // start time for the simulation
 const double END{(double)DAY_SEC * 30};  // end time for the simulation
 
+// tests a load balancing algorithm with 'nodes', 'num_iter' times
 void test_lba(std::function<int(std::vector<ServiceNode>)> lba,
-              std::vector<ServiceNode> nodes, int num_iters = 25);
+              std::vector<ServiceNode> nodes, int num_iters = 25) {
+  for (int ii = 0; ii < num_iters; ii++) {
+    std::cout << "Job index: " << ii << " | "
+              << "Chosen node: " << lba(nodes) << std::endl;
+  }
+}
+
 double getArrival();
+std::vector<ServiceNode> buildSQMS(int nNodes);
 
 int main() {
   std::vector<ServiceNode> nodes = {};
   for (int ii = 0; ii < 10; ii++) {
     nodes.push_back(ServiceNode(ii));
+  }
+  // build sqms
+  std::vector<ServiceNode> sqms(buildSQMS(10));
+  for (auto i = sqms.begin(); i != sqms.end(); i++) {
+    std::cout << *i << std::endl;
   }
 
   // generic LBA and ServiceNode test
@@ -34,14 +47,6 @@ int main() {
   test_lba(lba::random, nodes, 50);
 }
 
-// tests a load balancing algorithm with 'nodes', 'num_iter' times
-void test_lba(std::function<int(std::vector<ServiceNode>)> lba,
-              std::vector<ServiceNode> nodes, int num_iters = 25) {
-  for (int ii = 0; ii < num_iters; ii++) {
-    std::cout << "Job index: " << ii << " | "
-              << "Chosen node: " << lba(nodes) << std::endl;
-  }
-}
 
 // get a service time for a job
 // NOTE: This makes no sense
@@ -51,4 +56,23 @@ double getArrival() {
   prevArr += st; // update the the 
 
   return st;
+}
+
+/**
+ * @brief Build a single-queue, multi-server model
+ * 
+ * Add the nodes to the list of ServiceNodes for a model with no queues and all
+ * servers.
+ * 
+ * @param nNodes Number of ServiceNodes in the simulation.
+ * @return std::vector<ServiceNode> The ServiceNodes in the model
+ */
+std::vector<ServiceNode> buildSQMS(int nNodes) {
+  std::vector<ServiceNode> sqms;
+
+  for (int id = 0; id < nNodes; id++) {
+    sqms.push_back(ServiceNode(id));
+  }
+
+  return sqms;
 }
